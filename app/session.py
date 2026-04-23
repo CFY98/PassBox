29c01f03c1c74e810af3d15f03d2ffa7fa57755b
CFY98 <232689414+CFY98@users.noreply.github.com>
2026-04-23 20:05:12 +0100

    feat: Session lazy-loads enc_key

--- app/session.py
+++ app/session.py
@@ -1,17 +1,23 @@
 # PASSBOX MODULES
 import core.auth 
-from core.security import derive_hmac_key
+from core.security import (derive_hmac_key, derive_enc_key)
 
 # USER SESSION
 class Session:
-    def __init__(self, auth, vault_key, vault_file):
-        self.auth = auth
-        self.vault_key = vault_key
+    def __init__(self, master_key, vault_file):
+        self.master_key = master_key
         self.vault_file =  vault_file
         self._hmac_key = None
+        self._enc_key = None
     
     @property
     def hmac_key(self):
         if self._hmac_key is None:
-            self._hmac_key = derive_hmac_key(self.vault_key)
+            self._hmac_key = derive_hmac_key(self.hmac_key)
+        return self._hmac_key
+
+    @property
+    def enc_key(self):
+        if self._enc_key is None:
+            self._enc_key = derive_enc_key(self.enc_key)
         return self._hmac_key
