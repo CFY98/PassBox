b8df982f460548e20b51c70604d0f74a83d14b8f
CFY98 <232689414+CFY98@users.noreply.github.com>
2026-04-24 23:19:32 +0100

    refactor: moved confirm variable in separate function style: added new line breaks to prompts and messages

--- core/utils.py
+++ core/utils.py
@@ -34,18 +34,19 @@ def update_hint(username, hint):
 
 
 def ask_update_hint():
-    if pwinput.pwinput("Update hint (y/n)? ").strip().casefold() != "y":
+    if input("\nUpdate hint (y/n)? ").strip().casefold() != "y":
         return None
 
-    hint = input("Enter hint: ").strip()
+    hint = input("\nEnter hint: ").strip()
     return hint if hint else None
 
 
 def apply_hint_update(username):
     new_hint = ask_update_hint()
     if not new_hint:
-        return False
+        return None
     update_hint(username, new_hint)
+    return new_hint
 
 
 # PASSWORD UTILITIES
@@ -75,23 +76,27 @@ def strong_password(length=15):
     return "".join(characters)
 
 
+def confirm_new_pass(new_password):
+    confirm = pwinput.pwinput("Please confirm your new password: ")
+    if confirm != new_password:
+        print("\nPasswords do not match.")
+        return False
+    return True
+
+
 def get_password():
     suggest_pwd = strong_password(15)
 
     print(f"\nPassword suggestion: {suggest_pwd}")
     if pwinput.pwinput("\nUse suggestion (y/n)? ").strip().casefold() == "y":
         return suggest_pwd
-    password = pwinput.pwinput("Please enter the password: ")
+    password = pwinput.pwinput("\nPlease enter the password: ")
     return password
-
-
-def confirm_new_pass():
-    return pwinput.pwinput("Please confirm your new password: ")
 
 
 def prompt_pass_change():
     while True:
-        change = input("Change password (y/n)? ").strip().casefold()
+        change = input("\nChange password (y/n)? ").strip().casefold()
         match change:
             case "y":
                 return True
@@ -126,9 +131,9 @@ def update_password(username, new_password):
 
 def change_password(username, password):
     while True:
-        to_change = prompt_pass_change()
-        if not to_change:
-            print("The password was not updated")
+        change = prompt_pass_change()
+        if not change:
+            print("\nThe password was not updated")
             return password
 
         new_password = get_password()
@@ -136,29 +141,27 @@ def change_password(username, password):
             return password
 
         if new_password == password:
-            print("Cannot be old password, please try again.")
+            print("\nCannot be old password, please try again.")
             continue
 
         if new_password == "h3110 w0r1d!!":
+            if not confirm_new_pass(new_password):
+                continue
             update_password(username, new_password)
-            apply_hint_update(username)
+
             print(
-                "Thanks for using this easter egg, the password was successfully updated."
+                "\nThanks for using this easter egg, the password was successfully updated."
             )
             return new_password
 
         if not valid_password(new_password):
             print(
-                "Password must be between 5-15 characters long, with a minimum of 2 letters, numbers and symbols."
+                "\nPassword must be between 5-15 characters long, with a minimum of 2 letters, numbers and symbols."
             )
             continue
 
-        confirm = confirm_new_pass()
-        if confirm != new_password:
-            print("Passwords do not match. Try again.")
+        if not confirm_new_pass(new_password):
             continue
-
         update_password(username, new_password)
-        apply_hint_update(username)
-        print("The password was succesfully updated")
+        print("\nThe password was succesfully updated")
         return new_password
