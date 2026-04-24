8402a9f2dcf4396ff37663fc0d261d0c9d28ebfe
CFY98 <232689414+CFY98@users.noreply.github.com>
2026-04-24 17:37:10 +0100

    refactor: removed unused auth module

--- app/session.py
+++ app/session.py
@@ -1,15 +1,15 @@
 # PASSBOX MODULES
-import core.auth 
-from core.security import (derive_hmac_key, derive_enc_key)
+from core.security import derive_enc_key, derive_hmac_key
+
 
 # USER SESSION
 class Session:
     def __init__(self, master_key, vault_file):
         self.master_key = master_key
-        self.vault_file =  vault_file
+        self.vault_file = vault_file
         self._hmac_key = None
         self._enc_key = None
-    
+
     @property
     def hmac_key(self):
         if self._hmac_key is None:
