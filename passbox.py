4dba09d3ecbabe4c9cd7007c2374c4c7300e869d
CFY98 <232689414+CFY98@users.noreply.github.com>
2026-04-24 17:02:33 +0100

    refactor: cleaned up nested statements

--- passbox.py
+++ passbox.py
@@ -11,10 +11,12 @@ def load_auth():
     return Auth()
 
 
-def login_details():
-    login_name = input("Please enter your username: ").strip()
-    login_password = pwinput.pwinput("Please enter your password: ").strip()
-    return (login_name, login_password)
+def login_name():
+    return input("Please enter your username: ").strip()
+
+
+def login_password():
+    return pwinput.pwinput("Please enter your password: ").strip()
 
 
 def registration():
@@ -25,16 +27,26 @@ def register_hint():
     return input("Please enter a memorable hint: ")
 
 
+def enter_passbox(session):
+    print("\nEntering PassBox...")
+    main(session)
+
+
 # LOGIN DAEMON
 def passbox():
     max_attempts = 6
     failures = 0
 
     auth = load_auth()
-    username, password = login_details()
+    username = login_name()
 
     while failures < max_attempts:
+        password = login_password()
         status, session = auth.login(username, password)
+
+        if status == "success":
+            enter_passbox(session)
+            return
 
         if status == "invalid_user":
             if not registration():
@@ -42,27 +54,29 @@ def passbox():
             hint = register_hint()
             if auth.register(username, password, hint):
                 _, session = auth.login(username, password)
-                print("\nEntering PassBox...")
-                main(session)
+                enter_passbox(session)
             return
 
         elif status == "invalid_password":
             failures += 1
             attempts_left = max_attempts - failures
-            if attempts_left > 0:
-                print(f"Please try again. Attempts left: {attempts_left}".upper())
-                if failures >= 2:
-                    print(auth.get_hint(username))
-                if failures >= 4:
-                    new_password = auth.change_password(username, password)
-                    if new_password != password:
-                        password = new_password
-            else:
+
+            if attempts_left == 0:
                 print("Incorrect password, no attempts left.")
                 return
+
+            print(f"Please try again. Attempts left: {attempts_left}".upper())
+
+            if failures >= 2:
+                print(auth.get_hint(username))
+
+            if failures >= 4:
+                new_password = auth.change_password(username, password)
+                if not new_password:
+                    return password
+
         else:
-            print("\nEntering PassBox...")
-            main(session)
+            enter_passbox(session)
             return
 
 
