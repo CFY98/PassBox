644d1c99416e30aaa2a6c96a3044c4177627eaf3
CFY98 <232689414+CFY98@users.noreply.github.com>
2026-04-24 17:19:55 +0100

    fix: wrapped login-loop with an outer loop to re-prompt username when registration is rejected

--- passbox.py
+++ passbox.py
@@ -34,50 +34,53 @@ def enter_passbox(session):
 
 # LOGIN DAEMON
 def passbox():
-    max_attempts = 6
-    failures = 0
-
     auth = load_auth()
-    username = login_name()
 
-    while failures < max_attempts:
-        password = login_password()
-        status, session = auth.login(username, password)
+    while True:
+        max_attempts = 6
+        failures = 0
 
-        if status == "success":
-            enter_passbox(session)
-            return
+        username = login_name()
 
-        if status == "invalid_user":
-            if not registration():
-                return
-            hint = register_hint()
-            if auth.register(username, password, hint):
-                _, session = auth.login(username, password)
+        while failures < max_attempts:
+            password = login_password()
+            status, session = auth.login(username, password)
+
+            if status == "success":
                 enter_passbox(session)
-            return
-
-        elif status == "invalid_password":
-            failures += 1
-            attempts_left = max_attempts - failures
-
-            if attempts_left == 0:
-                print("Incorrect password, no attempts left.")
                 return
 
-            print(f"Please try again. Attempts left: {attempts_left}".upper())
+            if status == "invalid_user":
+                if not registration():
+                    break
 
-            if failures >= 2:
-                print(auth.get_hint(username))
+                hint = register_hint()
+                if auth.register(username, password, hint):
+                    _, session = auth.login(username, password)
+                    enter_passbox(session)
+                return
 
-            if failures >= 4:
-                new_password = auth.change_password(username, password)
-                if not new_password:
-                    return password
+            elif status == "invalid_password":
+                failures += 1
+                attempts_left = max_attempts - failures
 
-        else:
-            enter_passbox(session)
-            return
+                if attempts_left == 0:
+                    print("Incorrect password, no attempts left.")
+                    return
+
+                print(f"Please try again. Attempts left: {attempts_left}".upper())
+
+                if failures >= 2:
+                    print(auth.get_hint(username))
+
+                if failures >= 4:
+                    new_password = auth.change_password(username, password)
+                    if not new_password:
+                        return password
+
+            else:
+                enter_passbox(session)
+                return
 
 
 if __name__ == "__main__":
